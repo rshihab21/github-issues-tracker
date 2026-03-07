d66392b5f2bc6714bf3964e2a153479b81f36a7d
Shohel Rana Shihab <213002229@student.green.ac.bd>
2026-03-08 03:48:14 +0600

    fix conditional logic at card section

--- scripts/index.js
+++ scripts/index.js
@@ -39,24 +39,43 @@ function displayIssues() {
             `bg-white p-4 rounded shadow border-t-4 ${borderColor} cursor-pointer hover:shadow-lg transition`
 
         card.innerHTML = `
-
-<h3 class="font-semibold text-md">${issue.title}</h3>
-
-<p class="text-sm text-gray-500 mt-2">
-${issue.description.slice(0, 80)}...
-</p>
-
-<div class="flex justify-between items-center mt-4 text-sm">
-
-<span class="bg-gray-200 px-2 py-1 rounded">
-${issue.priority}
-</span>
-
-<span class="text-gray-500">
-${issue.author}
-</span>
-
-</div>
+            <div class="flex justify-between items-start">
+            <div class="w-8 h-8 rounded-full flex items-center justify-center
+            ${issue.status === "open" ? "bg-green-100 text-green-600" : "bg-purple-100 text-purple-600"}">
+           ${issue.priority === "high" || issue.priority === "medium"
+                ? '<img src="./assets/Open-Status.png"/>'
+                : '<img src="./assets/close-status.png"/>'}
+            
+           
+            </div>
+            <span class="text-xs px-3 py-1 rounded-full font-semibold 
+            ${issue.priority === "high"
+                ? "bg-[#FEECEC] text-[#F15959]"
+                : issue.priority === "medium"
+                    ? "bg-[#FFF6D1] text-[#F6A924]"
+                    : "bg-gray-200 text-gray-600"
+            }">
+            ${issue.priority}
+            </span>
+            </div>
+            <h3 class="font-semibold text-lg mt-3">
+            ${issue.title}
+            </h3>
+            <p class="text-gray-500 text-sm mt-2">
+            ${issue.description.slice(0, 90)}...
+            </p>
+            <div class="flex gap-2 mt-3">
+            <span class="text-xs px-3 py-1 rounded-full border border-red-200 text-red-500 bg-red-50">
+            🐞 BUG
+            </span>
+            <span class="text-xs px-3 py-1 rounded-full border border-yellow-300 text-yellow-600 bg-yellow-50">
+            🧑 HELP WANTED
+            </span>
+            </div>
+            <div class="border-t mt-4 pt-3 text-sm text-gray-500">
+            <p>#${issue.id || 1} by ${issue.author}</p>
+            <p>${new Date(issue.createdAt).toLocaleDateString()}</p>
+            </div>
 
 `
 
