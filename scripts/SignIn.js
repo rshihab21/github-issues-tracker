47f949cf264613711e22b72445f944c8c9362919
Shohel Rana Shihab <213002229@student.green.ac.bd>
2026-03-08 01:09:34 +0600

    Design Sign In form with functionality according to figma and assignment requirement

---
+++ scripts/SignIn.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("signInForm");
+const message =document.getElementById("message")
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    const userName=document.getElementById("username").value;
+    const password =document.getElementById("password").value;
+
+    if(userName==="admin" && password==="admin123") {
+        window.location.href="index.html"
+    }else {
+        message.innerText="Invalid username or password";
+        message.className = "text-red-600 text-center mt-4";
+    }
+})
