17238679a6ec6c081c98e4e9f224df9ca94ca0f5
Shohel Rana Shihab <213002229@student.green.ac.bd>
2026-03-08 15:10:15 +0600

    Desian modal and add popup functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
 const container = document.getElementById("issuesContainer")
 const loader = document.getElementById("loader")
-const issueCount = document.getElementById("issueCount")
+const issueCount = document.getElementById("issueCount");
+const modal = document.getElementById("issueModal")
+
 
 let issues = []
 let currentTab = "all"
+
 
 
 async function loadIssues() {
@@ -19,6 +22,7 @@ async function loadIssues() {
 function displayIssues() {
     container.innerHTML = ""
     let filtered = issues
+
     if (currentTab === "open") {
         filtered = issues.filter(issue => issue.status === "open")
     }
@@ -39,7 +43,7 @@ function displayIssues() {
             `bg-white p-4 rounded shadow border-t-4 ${borderColor} cursor-pointer hover:shadow-lg transition`
 
         card.innerHTML = `
-            <div class="flex justify-between items-start">
+            <div id="modal_id" class="flex justify-between items-start">
             <div class="w-8 h-8 rounded-full flex items-center justify-center
             ${issue.status === "open" ? "bg-green-100 text-green-600" : "bg-purple-100 text-purple-600"}">
            ${issue.priority === "high" || issue.priority === "medium"
@@ -65,12 +69,8 @@ function displayIssues() {
             ${issue.description.slice(0, 90)}...
             </p>
             <div class="flex gap-2 mt-3">
-            <span class="text-xs px-3 py-1 rounded-full border border-red-200 text-red-500 bg-red-50">
-            🐞 BUG
-            </span>
-            <span class="text-xs px-3 py-1 rounded-full border border-yellow-300 text-yellow-600 bg-yellow-50">
-            🧑 HELP WANTED
-            </span>
+            <div class="flex gap-2 ">${issue.labels.map(label => `<span class="bg-[#FFF8DB] border border-yellow-500 rounded-3xl p-2">${label}</span>`).join("")}</div>
+           
             </div>
             <div class="border-t mt-4 pt-3 text-sm text-gray-500">
             <p>#${issue.id || 1} by ${issue.author}</p>
@@ -78,7 +78,9 @@ function displayIssues() {
             </div>
 
 `
-
+        card.addEventListener("click", () => {
+            openModal(issue)
+        })
         container.appendChild(card)
 
     })
@@ -106,14 +108,37 @@ document.querySelectorAll(".tabBtn").forEach(btn => {
 
 document.getElementById("searchBtn").onclick = async () => {
     const text = document.getElementById("searchInput").value.trim().toLowerCase();
-     loader.classList.remove("hidden");
+    loader.classList.remove("hidden");
     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`)
     const data = await res.json()
     issues = data.data
     displayIssues()
     loader.classList.add("hidden")
-    
+
 }
+
+
+function openModal(issue) {
+    document.getElementById("modalTitle").innerText = issue.title
+    document.getElementById("modalDescription").innerText = issue.description
+    document.getElementById("modalAuthor").innerText = issue.author
+    document.getElementById("modalDate").innerText =
+        new Date(issue.createdAt).toLocaleDateString()
+    document.getElementById("modalStatus").innerText = issue.status
+
+    document.getElementById("modalAssignee").innerText = issue.author
+    document.getElementById("modalPriority").innerText = issue.priority
+
+    const labelsContainer = document.getElementById("modalLabels")
+    labelsContainer.innerHTML = issue.labels
+        .map(label => `<span class="badge badge-warning">${label}</span>`)
+        .join("")
+
+    modal.showModal()
+
+}
+
+
 
 
 
